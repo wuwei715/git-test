d2f1e112d14fd405a0ebd618c360b6fe08a454bd
wuwei715 <811479906@qq.com>
2022-06-14 09:04:53 +0800

    push commit

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -5,5 +5,6 @@ public class Person {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("push git");
     }
 }
