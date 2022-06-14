846e62854940308e6043e88e92f119f2c0636dc5
wuwei715 <811479906@qq.com>
2022-06-14 08:51:35 +0800

    first commmit

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Person {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
